f53d8c47521799536b76ced4f03f6f97403d2af7
Rafael Rezyapov <Rezograf@gmail.com>
2023-02-26 22:15:36 +0300

    add dialog w/o editing

--- Main.py
+++ Main.py
@@ -2,24 +2,21 @@ import datetime
 import json
 
 
-
-
-#BD=[12345,true,"яблоко",{"Миша": [898981646,464654654] }]
-#идентификатор, заголовок, тело заметки и дату/время создания или последнего изменения заметки.
-#pythnotic_name = load('pythnotic.json')
-pythnotic_name = 'pythnotic.json'
-
-notes = {
-    "first record":
-        {"body": "this is example for the note",
-        "date": "21.2.21 17:05"},
-    "second record":
-        {"body": "2. this is example for the note",
-        "date": "23.2.21 11:45"}
-}
+# notes = {
+# 1: 
+#     {"title": "first record",
+#     "body": "3.this is example fo the note",
+#     "date": "2023-02-24 20.01.08"
+#     },
+# 2:
+#     {"title":"second record",
+#     "body": "2. this is example for the note",
+#     "date": "2023-02-25 20.01.08"
+#     }
+# }
  
-def save(pythnotic_name, save_mode):
-    with open(pythnotic_name, mode=save_mode, encoding='utf-8') as file:
+def save(pythnotic_name):
+    with open(pythnotic_name,'w',encoding='utf-8') as file:
         file.write(json.dumps(notes, ensure_ascii=False))
 
 
@@ -28,101 +25,60 @@ def load(l_file):
         lst = json.load(file)
     return lst
 
-
-
-# for n in note:
-#     print (n)
-
-
-
-# print(notes.get('first record',0))
-# print(notes.get('second record',0))
-
-# print(notes.get('second record',0).get('body'))
-# print(notes.get('second record',0).get('date'))
-
-
-
-# print('----------------')
-# print(datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S"))
-
-
-    
-# print(note['second record'])
-# print(note.get('second record',0))
-
-notes = load(pythnotic_name)
+#notes = load(pythnotic_name)
 
 def show(notes):
     sorted_notes = sorted(notes.items(), key=lambda x: x[1].get('date'),reverse = True)
     for k,v in sorted_notes:
         print('---------------------------------------------------------------------------')
-        print('title: ',k)
+        print('ID: ',k)
+        print('title: ', v.get('title'))
         print('body: ', v.get('body'))
         print('date: ', v.get('date'))
+        print('---------------------------------------------------------------------------')
 
-def add(title, body):
-    notes[title] = {"body": body,
+def add(id, title, body):
+    notes[id] = {"title": title, "body": body,
         "date": datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")}
     print('\n---> note with title: "',title,'" is added\n')
     
-def delete(title):
-    if notes.get(title) != None: notes.pop(title)
-    print('\n---> note with title: "',title,'" is deleted\n')
+def delete(id):
+    if notes.get(id) != None: 
+        n = notes.pop(id)
+        print('\n---> note with title: "',n.get('title'),'" is deleted\n')
+    else:
+        print('note with id=',id,'not found')
     print(sorted(notes.items(), key=lambda x: x[1].get('date')))
     
-def edit(title,msg):
-    print(title,msg,"deleted")
+# def edit(title,msg):
+#     print(title,msg,"deleted")
+
+def dialog_help():
+    print("\nчтение - 1\nредактирование - 2\nдобавление - 3\nудаление заметок - 4\nсохранение - 5\nвыход без сохранения - 6")
     
-show(notes)
-delete('second record')
-add('4-th record', "это пример добавления треtьей строки")
-show(notes)
-
- 
-# Opening JSON file
-
-# with open('data.json') as json_file:
-#     data = json.load(json_file)
- 
-#     # for reading nested data [0] represents
-#     # the index value of the list
-#     print(data['people1'][0])
-     
-#     # for printing the klue pair of
-#     # for printing the key-value pair of
-#     # nested dictionary for loop can be used
-#     print("\nPrinting nested dictionary as a key-value pair\n")
-#     for i in data['people1']:
-#         print("Name:", i['name'])
-#         print("Website:", i['website'])
-#         print("From:", i['from'])
-#         print()
-
-#сохранением, чтением, добавлением, редактированием и удалением заметок
-#python notes.py add --title "новая заметка" –msg "тело новой заметки"
+pythnotic_name = 'pythnotic.json'
+notes = load(pythnotic_name) 
+action = '1'   
+while action != '6':
+    if action == '1':
+        show(notes)
+    if action == '2':
+        id = input("\n введите id редактируемой строки ...:")
+        print(notes[id].get('title'))
+        print(notes[id].get('body'))
+        #add(add(4,'3 record', "это пример добавления третьей строки"))
+    if action == '3':
+        add(4,'3 record', "это пример добавления третьей строки")
+    if action == '4':
+        delete(input("\n введите id удаляемой строки ...:"))
+    if action == '5':
+        save(pythnotic_name)        
+    if action == '6':
+        save(pythnotic_name)
+    dialog_help()  
+    action = input("\n Сделайте Ваш выбор ...:")
+      
 
 
-# dict.clear() - очищает словарь.
 
-# dict.copy() - возвращает копию словаря.
 
-# classmethod dict.fromkeys(seq[, value]) - создает словарь с ключами из seq и значением value (по умолчанию None).
-
-# dict.get(key[, default]) - возвращает значение ключа, но если его нет, не бросает исключение, а возвращает default (по умолчанию None).
-
-# dict.items() - возвращает пары (ключ, значение).
-
-# dict.keys() - возвращает ключи в словаре.
-
-# dict.pop(key[, default]) - удаляет ключ и возвращает значение.
-# Если ключа нет, возвращает default (по умолчанию бросает исключение).
-
-# dict.popitem() - удаляет и возвращает пару (ключ, значение). 
-# Если словарь пуст, бросает исключение KeyError. Помните, что словари неупорядочены.
-
-# dict.setdefault(key[, default]) - возвращает значение ключа, но если его нет, не бросает исключение, а создает ключ со значением default (по умолчанию None).
-
-# dict.update([other]) - обновляет словарь, добавляя пары (ключ, значение) из other. Существующие ключи перезаписываются. Возвращает None (не новый словарь!).
-
-# dict.values() - возвращает значения в словаре.
